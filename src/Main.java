96438d5ae0d9088358201c56803643df8b149f34
Semen <semen.dolgix@yandex.ru>
2024-05-16 20:52:04 +0500

    Task1,2

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Напишите код, с помощью которого можно напечатать только нечетные числа в консоль. ");
-        List<Integer> nums = new ArrayList<>(List.of(100, -1, 26, 3, 48, 4578, 5, 5, 6, 7));
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
         Numbers numbers=new Numbers();
         System.out.println(numbers.oddNumber(nums));
 
